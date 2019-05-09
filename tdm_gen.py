a2e41a599dea2b2591a5b0241b60054154fac0c9
stgatilov <none@none>
2019-05-10 00:56:06 +0700

    Added a script which generates all code for TDM.

---
+++ tdm_gen.py
@@ -0,0 +1,18 @@
+import os, re
+
+g_output_dir = '../darkmod_src/renderer'
+
+for spec in ['gl', 'wgl', 'glx']:
+    parts = [
+        'python -m glad',
+        '--spec="%s"' % spec,
+        '--profile="compatibility"',
+        '--reproducible',
+        '--generator="c"',
+        '--omit-khrplatform',
+        '--local-files',
+        '--out-path=%s' % g_output_dir,
+    ]
+    cmd = ' '.join(parts);
+    print(cmd)
+    os.system(cmd)
