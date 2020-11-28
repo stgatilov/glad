e04cb3f9429bebf438bde9fb69d9d69ee91ea807
stgatilov <none@none>
2020-11-28 14:41:37 +0700

    Always define _GLAD_IS_SOME_NEW_VERSION.
    Note that TDM runs both in core and compatibility profiles, depending on settings.

--- glad/lang/c/loader/gl.py
+++ glad/lang/c/loader/gl.py
@@ -20,9 +20,9 @@ int gladLoadGL(void) {
 _OPENGL_HAS_EXT = '''
 struct gladGLversionStruct GLVersion = { 0, 0 };
 
-#if defined(GL_ES_VERSION_3_0) || defined(GL_VERSION_3_0)
+//#if defined(GL_ES_VERSION_3_0) || defined(GL_VERSION_3_0)
 #define _GLAD_IS_SOME_NEW_VERSION 1
-#endif
+//#endif
 
 static int max_loaded_major;
 static int max_loaded_minor;
